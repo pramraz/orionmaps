4a30607b2cd4f94c5c3cb10e45b73ba3f020312b
pramraz <pramraz@gmail.com>
2026-09-22 21:36:09 +0200

    version code update (same as last commit)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 37
         versionCode = getGitCommitCount()
-        versionName = "1.2"
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
